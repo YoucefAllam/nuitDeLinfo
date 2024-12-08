5d87ceabb8cbec67e3aa3a08eb45d8781b933815
Youcef Allam <youcefallam71@gmail.com>
2024-12-09 00:44:39 +0100

    pushing all files

---
+++ src/test/java/com/nuitInfo/nuitInfo/NuitInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuitInfo.nuitInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuitInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
